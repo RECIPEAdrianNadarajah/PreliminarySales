b16039a57867a445a77a8d901a84b4ebef21c383
Adrian Nadarajah <anadarajah@recipeunlimited.com>
2023-03-04 17:26:28 -0500

    querying from looker_ready.recipe_sales_yoy

--- app.py
+++ app.py
@@ -17,7 +17,27 @@ st.markdown(hide_streamlit_style, unsafe_allow_html = True)
 st.title("RECIPE Preliminary Sales")
 
 ##Create API Client - to connect into Advanced Data Analytics project in BigQuery
-
 st.cache_resource()
 bqCredentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
 client = bigquery.Client(credentials = bqCredentials)
+
+##GCP Queries
+dailyTotalsQuery = ('''
+               SELECT *
+               FROM `rcp-ada.looker_ready.recipe_sales_yoy`
+               WHERE DATE(business_date) <= CURRENT_DATE()-1
+               ORDER BY DATE(business_date) DESC, store_number DESC, rvcID ASC
+''')
+
+##Pipeline Functions##
+######################
+
+st.cache_data(ttl = 86400)
+def runQuery(query):
+    query_job = client.query(query)
+    return query_job.to_dataframe()
+
+
+dailyTotals = runQuery(dailyTotalsQuery)
+
+st.dataframe(dailyTotals)
